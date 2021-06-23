60d24798dbe0f487dc5ee2590ac5fb7f78b5f64a
Davidc794 <86282166+Davidc794@users.noreply.github.com>
2021-06-22 22:37:30 -0600

    Primer Commit
    Primera Clase de arreglos

---
+++ Programacion_3_1902_2021/Programacion_3_1902_2021/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Programacion_3_1902_2021
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Ejecutarbutton = new System.Windows.Forms.Button();
+            this.DiaslistBox = new System.Windows.Forms.ListBox();
+            this.SemanalistBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // Ejecutarbutton
+            // 
+            this.Ejecutarbutton.Location = new System.Drawing.Point(34, 12);
+            this.Ejecutarbutton.Name = "Ejecutarbutton";
+            this.Ejecutarbutton.Size = new System.Drawing.Size(134, 79);
+            this.Ejecutarbutton.TabIndex = 0;
+            this.Ejecutarbutton.Text = "Ejecutar";
+            this.Ejecutarbutton.UseVisualStyleBackColor = true;
+            this.Ejecutarbutton.Click += new System.EventHandler(this.Ejecutarbutton_Click);
+            // 
+            // DiaslistBox
+            // 
+            this.DiaslistBox.FormattingEnabled = true;
+            this.DiaslistBox.Location = new System.Drawing.Point(12, 128);
+            this.DiaslistBox.Name = "DiaslistBox";
+            this.DiaslistBox.Size = new System.Drawing.Size(156, 121);
+            this.DiaslistBox.TabIndex = 1;
+            // 
+            // SemanalistBox
+            // 
+            this.SemanalistBox.FormattingEnabled = true;
+            this.SemanalistBox.Location = new System.Drawing.Point(187, 128);
+            this.SemanalistBox.Name = "SemanalistBox";
+            this.SemanalistBox.Size = new System.Drawing.Size(159, 121);
+            this.SemanalistBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 261);
+            this.Controls.Add(this.SemanalistBox);
+            this.Controls.Add(this.DiaslistBox);
+            this.Controls.Add(this.Ejecutarbutton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Ejecutarbutton;
+        private System.Windows.Forms.ListBox DiaslistBox;
+        private System.Windows.Forms.ListBox SemanalistBox;
+    }
+}
+
---
+++ Programacion_3_1902_2021/Programacion_3_1902_2021/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programacion_3_1902_2021
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Ejecutarbutton_Click(object sender, EventArgs e)
+        {
+            //arreglos 
+            int [] Arra1 = new int [ 5 ];
+
+            Arra1[0] = 10;
+            Arra1[1] = 20;
+            Arra1[2] = 30;
+            Arra1[3] = 40;
+            Arra1[4] = 50;
+            //----------------------------------------------
+            int [] array2 = new int[] { 1, 2, 3, 4, 5 };
+            //---------------------------------------------
+            int[] array3 = { 1, 2, 3, 4, 5 };
+
+            string[] diasSemana = new string[] { "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado","Domingo" };
+
+          
+
+            for (int i= 0; i < Arra1.Length; i++ )
+            {
+                DiaslistBox.Items.Add(Arra1[i]);
+            }
+
+            foreach (var y in diasSemana)
+            {
+                SemanalistBox.Items.Add(y);
+            }
+
+        }
+    }
+}
