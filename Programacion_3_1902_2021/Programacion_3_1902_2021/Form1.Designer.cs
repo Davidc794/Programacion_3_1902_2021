b9acd76c410d9ba5421681a3a5d4fc560d01e83c
Davidc794 <86282166+Davidc794@users.noreply.github.com>
2021-06-28 22:06:47 -0600

    segunda clase vectores
    vimos como ordenar arreglos y sacar valores max y min

--- Programacion_3_1902_2021/Programacion_3_1902_2021/Form1.Designer.cs
+++ Programacion_3_1902_2021/Programacion_3_1902_2021/Form1.Designer.cs
@@ -31,6 +31,9 @@
             this.Ejecutarbutton = new System.Windows.Forms.Button();
             this.DiaslistBox = new System.Windows.Forms.ListBox();
             this.SemanalistBox = new System.Windows.Forms.ListBox();
+            this.Ordenarbutton = new System.Windows.Forms.Button();
+            this.OrdenarlistBox = new System.Windows.Forms.ListBox();
+            this.MaxiMinbutton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Ejecutarbutton
@@ -48,22 +51,53 @@
             this.DiaslistBox.FormattingEnabled = true;
             this.DiaslistBox.Location = new System.Drawing.Point(12, 128);
             this.DiaslistBox.Name = "DiaslistBox";
-            this.DiaslistBox.Size = new System.Drawing.Size(156, 121);
+            this.DiaslistBox.Size = new System.Drawing.Size(105, 121);
             this.DiaslistBox.TabIndex = 1;
             // 
             // SemanalistBox
             // 
             this.SemanalistBox.FormattingEnabled = true;
-            this.SemanalistBox.Location = new System.Drawing.Point(187, 128);
+            this.SemanalistBox.Location = new System.Drawing.Point(150, 128);
             this.SemanalistBox.Name = "SemanalistBox";
-            this.SemanalistBox.Size = new System.Drawing.Size(159, 121);
+            this.SemanalistBox.Size = new System.Drawing.Size(127, 121);
             this.SemanalistBox.TabIndex = 2;
+            // 
+            // Ordenarbutton
+            // 
+            this.Ordenarbutton.Location = new System.Drawing.Point(208, 11);
+            this.Ordenarbutton.Name = "Ordenarbutton";
+            this.Ordenarbutton.Size = new System.Drawing.Size(120, 80);
+            this.Ordenarbutton.TabIndex = 3;
+            this.Ordenarbutton.Text = "Ordenar";
+            this.Ordenarbutton.UseVisualStyleBackColor = true;
+            this.Ordenarbutton.Click += new System.EventHandler(this.Ordenarbutton_Click);
+            // 
+            // OrdenarlistBox
+            // 
+            this.OrdenarlistBox.FormattingEnabled = true;
+            this.OrdenarlistBox.Location = new System.Drawing.Point(329, 128);
+            this.OrdenarlistBox.Name = "OrdenarlistBox";
+            this.OrdenarlistBox.Size = new System.Drawing.Size(112, 121);
+            this.OrdenarlistBox.TabIndex = 4;
+            // 
+            // MaxiMinbutton
+            // 
+            this.MaxiMinbutton.Location = new System.Drawing.Point(366, 11);
+            this.MaxiMinbutton.Name = "MaxiMinbutton";
+            this.MaxiMinbutton.Size = new System.Drawing.Size(103, 80);
+            this.MaxiMinbutton.TabIndex = 5;
+            this.MaxiMinbutton.Text = "MaximoMinim";
+            this.MaxiMinbutton.UseVisualStyleBackColor = true;
+            this.MaxiMinbutton.Click += new System.EventHandler(this.MaxiMinbutton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(601, 261);
+            this.Controls.Add(this.MaxiMinbutton);
+            this.Controls.Add(this.OrdenarlistBox);
+            this.Controls.Add(this.Ordenarbutton);
             this.Controls.Add(this.SemanalistBox);
             this.Controls.Add(this.DiaslistBox);
             this.Controls.Add(this.Ejecutarbutton);
@@ -78,6 +112,9 @@
         private System.Windows.Forms.Button Ejecutarbutton;
         private System.Windows.Forms.ListBox DiaslistBox;
         private System.Windows.Forms.ListBox SemanalistBox;
+        private System.Windows.Forms.Button Ordenarbutton;
+        private System.Windows.Forms.ListBox OrdenarlistBox;
+        private System.Windows.Forms.Button MaxiMinbutton;
     }
 }
 
--- Programacion_3_1902_2021/Programacion_3_1902_2021/Form1.cs
+++ Programacion_3_1902_2021/Programacion_3_1902_2021/Form1.cs
@@ -20,7 +20,7 @@ namespace Programacion_3_1902_2021
         private void Ejecutarbutton_Click(object sender, EventArgs e)
         {
             //arreglos 
-            int [] Arra1 = new int [ 5 ];
+            int[] Arra1 = new int[5];
 
             Arra1[0] = 10;
             Arra1[1] = 20;
@@ -28,15 +28,15 @@ namespace Programacion_3_1902_2021
             Arra1[3] = 40;
             Arra1[4] = 50;
             //----------------------------------------------
-            int [] array2 = new int[] { 1, 2, 3, 4, 5 };
+            int[] array2 = new int[] { 1, 2, 3, 4, 5 };
             //---------------------------------------------
             int[] array3 = { 1, 2, 3, 4, 5 };
 
-            string[] diasSemana = new string[] { "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado","Domingo" };
+            string[] diasSemana = new string[] { "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo" };
 
-          
 
-            for (int i= 0; i < Arra1.Length; i++ )
+
+            for (int i = 0; i < Arra1.Length; i++)
             {
                 DiaslistBox.Items.Add(Arra1[i]);
             }
@@ -47,5 +47,63 @@ namespace Programacion_3_1902_2021
             }
 
         }
-    }
+
+        private void Ordenarbutton_Click(object sender, EventArgs e)
+        {
+            int[] valores = new int[] { 3, 5, 1, 7, 8, 9, 6, 2 };
+            //ordenar de menor a mayor
+            // Array.Sort(valores);
+
+            //ordenar de mayor a menor 
+            //Array.Reverse(valores);
+
+
+            //----------------------------------------------------------
+
+
+            //valores = valores.OrderByDescending(x => x).ToArray();
+
+
+            //=---------------------------------------------------------------
+
+
+
+            foreach (var item in valores)
+            {
+                OrdenarlistBox.Items.Add(item);
+            }
+            OrdenarlistBox.Sorted = true;
+        }
+
+        private void MaxiMinbutton_Click(object sender, EventArgs e)
+        {
+            int[] valores = new int[] { 3, 5, 1, 7, 8, 9, 6, 2 };
+
+            Array.Sort(valores);
+            int minimo = valores[0];
+            // int maximo = valores[7];
+            int maximo = valores[valores.Length - 1];
+
+            MessageBox.Show("Valor Maximo :" + maximo + "/ Valor Minimo : " + minimo);
+
+
+
+           // int valorMaximo = valores[0];
+           // int valorMinimo = valores[0];
+
+            // for (int i = 1; i < valores.Length; i++)
+            //{
+            //  if (valores[i] > valorMaximo)
+            //{
+            //   valorMaximo = valores[i];
+            //}
+
+            //                if (valores[i] < valorMinimo)
+            ///              {
+            //               valorMinimo = valores[i];
+            //         }
+            //       MessageBox.Show("Valor Maximo :" + valorMaximo + "/ Valor Minimo : " + valorMinimo);
+     //   }
+        }  }
+
 }
